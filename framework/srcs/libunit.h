b924ddd539d2bd78ca577a0b5a9798bf959407fd
Yui Mizukami <ymizukam@c2r5s8.42tokyo.jp>
2025-05-25 14:50:30 +0900

    Linux Makefile

--- framework/srcs/libunit.h
+++ framework/srcs/libunit.h
@@ -1,27 +1,30 @@
 #ifndef LIBUNIT_H
-#define LIBUNIT_H
+# define LIBUNIT_H
 
-#include "libft.h"
+# include "libft.h"
+# include <signal.h>
+# include <unistd.h>
+# include <wait.h>
 
-#define RESET "\033[0m"
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
+# define RESET "\033[0m"
+# define RED "\033[31m"
+# define GREEN "\033[32m"
+# define YELLOW "\033[33m"
 
-#define TIMEOUT 5
+# define TIMEOUT 5
 
 typedef struct s_unittest
 {
-    char *name;
-    int (*func)(void);
-} t_unittest;
+	char	*name;
+	int		(*func)(void);
+}			t_unittest;
 
-void load_test(t_list **l, const char *name, int (*func)(void));
-int launch_tests(const char *func_name, t_list *l);
-void clean_tests(t_list **l);
+void		load_test(t_list **l, const char *name, int (*func)(void));
+int			launch_tests(const char *func_name, t_list *l);
+void		clean_tests(t_list **l);
 
 /*          Util          */
-void *test_new(const char *name, int (*func)(void));
-void test_free(void *test);
-void *xmalloc(size_t size);
+void		*test_new(const char *name, int (*func)(void));
+void		test_free(void *test);
+void		*xmalloc(size_t size);
 #endif
